0431ebfe86a63187508ada5b9b94723e64f28f41
Татьяна Смыслова <l.dalecrua@gmail.com>
2022-05-14 17:12:23 +0300

    задание

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import ru.netology.sqr.SQRService;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SQRService service = new SQRService();
+        int result = service.method(200,300);
+
+        System.out.println(result);
+    }
+}
